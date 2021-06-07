87f6befbccdda404057a2155db3f4afa709e4887
GNiessen01 <gniessen@roc-dev.com>
2021-06-07 17:11:05 +0200

    Player + Camera
    De speler kan nu lopen met A/< en  D/>.
    De speler kan nu springen met SPATIE .
    De camera beweegt nu met de speler mee.

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -9,6 +9,11 @@ public class Player : MonoBehaviour
     public Vector2 direction;
     private bool facingRight = true;
 
+    [Header("Vertical Movement")]
+    public float jumpSpeed = 15f;
+    public float jumpDelay = 0.25f;
+    private float jumpTimer;
+
     [Header("Components")]
     public Rigidbody2D rb;
     public Animator animator;
@@ -17,15 +22,34 @@ public class Player : MonoBehaviour
     [Header("Physics")]
     public float maxSpeed = 7f;
     public float linearDrag = 4f;
+    public float gravity = 1;
+    public float fallMultipier = 5f;
+
+    [Header("Collision")]
+    public bool onGround = false;
+    public float groundLength = 0.6f;
+    public Vector3 colliderOffset;
 
     // Update is called once per frame
     void Update()
     {
+        onGround = Physics2D.Raycast(transform.position + colliderOffset, Vector2.down, groundLength, groundLayer) || Physics2D.Raycast(transform.position - colliderOffset, Vector2.down, groundLength, groundLayer);
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            jumpTimer = Time.time + jumpDelay;
+        }
+
         direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
     }
      void FixedUpdate ()
     {
         moveCharacter(direction.x);
+
+        if (jumpTimer > Time.time && onGround)
+        {
+            Jump();
+        }
     }
     void moveCharacter(float horizontal)
     {
@@ -41,22 +65,48 @@ public class Player : MonoBehaviour
             rb.velocity = new Vector2(Mathf.Sign(rb.velocity.x) * maxSpeed, rb.velocity.y);
         }
     }
+    void Jump ()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, 0);
+        rb.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+    }
     void modifyPhysics()
     {
         bool changingDirections = (direction.x > 0 && rb.velocity.x < 0) || (direction.x < 0 && rb.velocity.x > 0);
-
-        if (Mathf.Abs(direction.x) < 0.4f || changingDirections)
+   
+        if (onGround)
         {
-            rb.drag = linearDrag;
+            if (Mathf.Abs(direction.x) < 0.4f || changingDirections)
+            {
+                rb.drag = linearDrag;
+            }
+            else
+            {
+                rb.drag = 0f;
+            }
+            rb.gravityScale = 0;
         }
-        else
-        {
-            rb.drag = 0f;
+        else { rb.gravityScale = gravity;
+            rb.drag = linearDrag * 0.15f;
+            if(rb.velocity.y < 0)
+            {
+                rb.gravityScale = gravity * fallMultipier;
+            } else if(rb.velocity.y > 0 && !Input.GetButton("Jump"))
+            {
+                rb.gravityScale = gravity * (fallMultipier / 2);
+            }
         }
     }
     void Flip()
     {
         facingRight = !facingRight;
         transform.rotation = Quaternion.Euler(0, facingRight ? 0 : 180, 0);
+    }
+    private void OnDrawGizmos ()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(transform.position + colliderOffset, transform.position + colliderOffset + Vector3.down * groundLength);
+        Gizmos.DrawLine(transform.position - colliderOffset, transform.position - colliderOffset + Vector3.down * groundLength);
+
     }
 }
